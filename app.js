5177d0afb4fc85540302e75455b731103c6e537c
vladislav <vl_evlashin@mail.ru>
2024-04-30 23:44:06 +0300

    part of adaptive

--- app.js
+++ app.js
@@ -19,3 +19,32 @@ for (let btn of menu_buttons) {
 menu_btn.addEventListener('click',() => {
     menu.classList.add('hide')
 })
+
+const screenWidth = window.screen.width
+
+const slider = document.querySelector('.main_goods_viewport')
+const backBtn = document.querySelector('.goods_back_btn')
+const nextBtn = document.querySelector('.goods_next_btn')
+let curLeft = 0
+const child = slider.children.length
+let valueSlide = screenWidth > 1440? 263 : 18.26
+let viewChild = 5
+let size = screenWidth > 1440? 'px' : 'vw'
+
+nextBtn.addEventListener('click', () => {
+    if (curLeft <= -child*valueSlide +viewChild*valueSlide) {
+        curLeft = 0
+    } else {
+        curLeft = curLeft - valueSlide
+    }
+    slider.style.left = curLeft + size
+})
+
+backBtn.addEventListener('click', () => {
+    if (curLeft >= 0) {
+        curLeft = -child*valueSlide +viewChild*valueSlide
+    } else {
+        curLeft = curLeft + valueSlide
+    }
+    slider.style.left = curLeft + size
+})
